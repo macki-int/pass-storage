98546d084a12f5641a833b7255e91f69d298dcc7
macki-int <macki@int.pl>
2021-07-28 00:11:00 +0200

    refactor fxml name

--- src/main/java/pl/mj/passstorage/PassStorage.java
+++ src/main/java/pl/mj/passstorage/PassStorage.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class PassStorage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PassStorage.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(PassStorage.class.getResource("pass-storage-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setTitle("Password Storage");
         stage.setScene(scene);
