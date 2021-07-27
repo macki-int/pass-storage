0f5b184372942b27a44fe31b42e8ffcc9041b31f
macki-int <macki@int.pl>
2021-07-28 00:07:17 +0200

    change width scen to 520px

--- src/main/java/pl/mj/passstorage/PassStorage.java
+++ src/main/java/pl/mj/passstorage/PassStorage.java
@@ -11,7 +11,7 @@ public class PassStorage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PassStorage.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 420, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 400);
         stage.setTitle("Password Storage");
         stage.setScene(scene);
         stage.show();
