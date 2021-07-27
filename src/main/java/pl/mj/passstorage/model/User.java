4a4039d4d0177157b573e2f794e69787ba0d91f5
macki-int <macki@int.pl>
2021-07-27 23:45:58 +0200

    add id field in user class

--- src/main/java/pl/mj/passstorage/model/User.java
+++ src/main/java/pl/mj/passstorage/model/User.java
@@ -1,8 +1,17 @@
 package pl.mj.passstorage.model;
 
 public class User {
+    private long id;
     private String name;
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
