d6bdadcadfeb6fefd878357e765675527cad16eb
macki-int <macki@int.pl>
2021-07-30 23:33:50 +0200

    remove field id

--- src/main/java/pl/mj/passstorage/model/Credential.java
+++ src/main/java/pl/mj/passstorage/model/Credential.java
@@ -1,17 +1,8 @@
 package pl.mj.passstorage.model;
 
 public class Credential {
-    private long id;
     private String name;
     private String password;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
