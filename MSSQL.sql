a01d654219585187cf40e23febf9aafa9802d966
macki-int <macki@int.pl>
2021-07-31 22:53:00 +0200

    fix syntax in sql file

--- MSSQL.sql
+++ MSSQL.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE [ps]
 
-CREATE TABLE [[passwords] (
-  [id] [uniqueidentifier] NOT NULL,
-	[name] [varchar](256) NOT NULL,
-	[url] [varchar](max) NULL,
-	[user] [varchar](256) NULL,
-	[password] [varchar](256) NULL,
-	[description] [varchar](max) NULL,
-	[active] [bit] NOT NULL,
-	[update_date] [date] NOT NULL,
- CONSTRAINT [PK_passwords] PRIMARY KEY CLUSTERED
-)
+CREATE TABLE passwords (
+  id uniqueidentifier NOT NULL,
+	name varchar(256) NOT NULL,
+	url varchar(max) NULL,
+	user varchar(256) NULL,
+	password varchar(256) NULL,
+	description varchar(max) NULL,
+	active bit NOT NULL,
+	update_date date NOT NULL,
+ PRIMARY KEY (PK_passwords)
+);
