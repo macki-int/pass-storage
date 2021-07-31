fde2e86a714978da4928725e3005fe2bffab350b
macki-int <macki@int.pl>
2021-07-31 22:35:52 +0200

    add Password class

---
+++ src/main/java/pl/mj/passstorage/model/Password.java
@@ -0,0 +1,78 @@
+package pl.mj.passstorage.model;
+
+import java.time.LocalDate;
+
+public class Password {
+    private String id;
+    private String name;
+    private String url;
+    private String user;
+    private String password;
+    private String description;
+    private boolean active;
+    private LocalDate updateDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+}
