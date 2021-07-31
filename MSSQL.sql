46143c58684d546cf5f61219ec083be466258035
macki-int <macki@int.pl>
2021-07-31 22:21:23 +0200

    change database schema

--- MSSQL.sql
+++ MSSQL.sql
@@ -1,31 +1,13 @@
-CREATE TABLE [tasks] (
-  [id] bigint NOT NULL,
-  [name] character varying(255) NOT NULL,
-  [colour] character varying(255) NOT NULL
+CREATE DATABASE [ps]
+
+CREATE TABLE [[passwords] (
+  [id] [uniqueidentifier] NOT NULL,
+	[name] [varchar](256) NOT NULL,
+	[url] [varchar](max) NULL,
+	[user] [varchar](256) NULL,
+	[password] [varchar](256) NULL,
+	[description] [varchar](max) NULL,
+	[active] [bit] NOT NULL,
+	[update_date] [date] NOT NULL,
+ CONSTRAINT [PK_passwords] PRIMARY KEY CLUSTERED
 )
-GO
-
-CREATE TABLE [events] (
-  [id] bigint NOT NULL,
-  [description] character varying(255),
-  [event_date] date,
-  [task_id] bigint NOT NULL,
-  [user_id] bigint NOT NULL
-)
-GO
-
-CREATE TABLE [users] (
-  [id] bigint NOT NULL,
-  [active] boolean,
-  [first_name] character varying(255),
-  [last_name] character varying(255),
-  [password] character varying(255) NOT NULL,
-  [role] integer NOT NULL
-)
-GO
-
-ALTER TABLE [events] ADD FOREIGN KEY ([task_id]) REFERENCES [tasks] ([id])
-GO
-
-ALTER TABLE [events] ADD FOREIGN KEY ([user_id]) REFERENCES [users] ([id])
-GO
