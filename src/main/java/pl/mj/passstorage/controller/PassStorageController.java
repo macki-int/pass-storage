cffdd66d63798e1d7a5528826f9b638f0c494b33
macki-int <macki@int.pl>
2021-08-03 00:04:45 +0200

    add method initialize

--- src/main/java/pl/mj/passstorage/controller/PassStorageController.java
+++ src/main/java/pl/mj/passstorage/controller/PassStorageController.java
@@ -2,17 +2,39 @@ package pl.mj.passstorage.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import pl.mj.passstorage.model.ServerConnectionSetting;
+import pl.mj.passstorage.service.ServerConfigJsonReadFromFile;
 
-public class PassStorageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PassStorageController  implements  Initializable{
+
     @FXML
     private Button buttonLogin;
+    @FXML
+    private TextField textUrl;
+    @FXML
+    private TextField textPort;
+    ServerConnectionSetting serverConnectionSetting;
 
     @FXML
     protected void onLoginButtonAction(ActionEvent event) {
         Stage stage = (Stage) buttonLogin.getScene().getWindow();
         stage.close();
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        serverConnectionSetting = new ServerConfigJsonReadFromFile().getServerConnectionSetting();
+
+        textUrl.setText(serverConnectionSetting.getUrl());
+        textPort.setText(serverConnectionSetting.getPort());
+    }
+
+
 }
