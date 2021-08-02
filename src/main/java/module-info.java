3088f11c7f52ab85add05ef81b06ba7cca348ca1
macki-int <macki@int.pl>
2021-08-03 00:09:18 +0200

    add requires for json and junit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,15 @@ module pl.mj.passstorage {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.naming;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires java.persistence;
     requires org.hibernate.orm.core;
+    requires org.json;
+    requires org.junit.jupiter.api;
 
     opens pl.mj.passstorage to javafx.fxml;
     exports pl.mj.passstorage;
