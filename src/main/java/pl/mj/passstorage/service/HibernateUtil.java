f7718d57b118ea09cd4df6eb4ddd472dc58cc18c
macki-int <macki@int.pl>
2021-08-03 00:00:20 +0200

    remove endline

--- src/main/java/pl/mj/passstorage/service/HibernateUtil.java
+++ src/main/java/pl/mj/passstorage/service/HibernateUtil.java
@@ -11,21 +11,26 @@ public class HibernateUtil {
     private static SessionFactory sessionFactory;
     private ServiceRegistry serviceRegistry;
 
+    static {
+        Configuration config = getConfiguration();
+        sessionFactory = config.buildSessionFactory();
+    }
+
 
     public static Session openSession(){
         return sessionFactory.openSession();
     }
     private static Configuration getConfiguration(){
-        Configuration confg = new Configuration();
-        confg.addAnnotatedClass(Password.class);
-        confg.setProperty("hibernate.connection.driver_class", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        confg.setProperty("hibernate.connection.url", "jdbc:sqlserver://localhost:1433;databaseName=ps");
-        confg.setProperty("hibernate.connection.username", "");
-        confg.setProperty("hibernate.connection.password", "");
-        confg.setProperty("hibernate.show_sql", "true");
-        confg.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
-        confg.setProperty("hibernate.hbm2ddl.auto", "update");
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(Password.class);
+        config.setProperty("hibernate.connection.driver_class", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        config.setProperty("hibernate.connection.url", "jdbc:sqlserver://localhost:1433;databaseName=ps");
+        config.setProperty("hibernate.connection.username", "passtorage");
+        config.setProperty("hibernate.connection.password", "Inora1234");
+        config.setProperty("hibernate.show_sql", "true");
+        config.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
+        config.setProperty("hibernate.hbm2ddl.auto", "update");
 
-        return confg;
+        return config;
     }
 }
