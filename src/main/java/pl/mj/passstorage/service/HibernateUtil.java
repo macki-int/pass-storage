a5ecf1ab5889a9f2fb5e30a84d876bd1b87c2c41
macki-int <macki@int.pl>
2021-08-07 23:33:38 +0200

    change hibernate session configuration in getSessionFactory method

--- src/main/java/pl/mj/passstorage/service/HibernateUtil.java
+++ src/main/java/pl/mj/passstorage/service/HibernateUtil.java
@@ -1,37 +1,41 @@
 package pl.mj.passstorage.service;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 import pl.mj.passstorage.model.Password;
+
+import java.util.Properties;
 
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
-//    private ServiceRegistry serviceRegistry;
 
-    static {
-        Configuration config = getConfiguration();
-        sessionFactory = config.buildSessionFactory();
-    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                settings.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=ps");
+                settings.put(Environment.USER, "passtorage");
+                settings.put(Environment.PASS, "Inora1234");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
+                Configuration config = new Configuration();
+                config.setProperties(settings);
+                config.addAnnotatedClass(Password.class);
 
-    public static Session openSession(){
-        return sessionFactory.openSession();
-    }
-
-    private static Configuration getConfiguration(){
-        Configuration config = new Configuration();
-        config.addAnnotatedClass(Password.class);
-        config.setProperty("hibernate.connection.driver_class", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        config.setProperty("hibernate.connection.url", "jdbc:sqlserver://localhost:1433;databaseName=ps");
-        config.setProperty("hibernate.connection.username", "passtorage");
-        config.setProperty("hibernate.connection.password", "Inora1234");
-        config.setProperty("hibernate.show_sql", "true");
-        config.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
-        config.setProperty("hibernate.hbm2ddl.auto", "update");
-
-        return config;
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+                sessionFactory = config.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
