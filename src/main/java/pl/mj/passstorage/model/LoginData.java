e1cc9e222cc06f0a63dd0243563f82b40c35dac7
macki-int <macki@int.pl>
2021-08-31 23:54:54 +0200

    rename class Password on LoginData

---
+++ src/main/java/pl/mj/passstorage/model/LoginData.java
@@ -0,0 +1,90 @@
+package pl.mj.passstorage.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "passwords")
+public class LoginData {
+    @Id
+    @Type(type = "uuid-char")
+    @Column(columnDefinition = "UNIQUEIDENTIFIER")
+    private UUID id;
+    private String name;
+    private String url;
+    private String user;
+    private String passHash;
+    private String description;
+    private boolean active;
+    private LocalDate updateDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassHash() {
+        return passHash;
+    }
+
+    public void setPassHash(String passHash) {
+        this.passHash = passHash;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+}
